8154dfc82239a2bb8f79869f7df7c945136d4261
Nick Cox <nicholas.cox@t-mark.com>
2020-01-07 10:43:48 -0500

    API call to get players

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    players: [],
   },
   mutations: {
+    SAVE_PLAYERS(state, players) {
+      state.players = players;
+    },
   },
   actions: {
+    loadPlayers({ commit }) {
+      axios.get('https://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2017&week=1&format=json').then((result) => {
+        commit('SAVE_PLAYERS', result.data.players);
+      }).catch((error) => {
+        throw new Error(`API ${error}`);
+      });
+    },
   },
   modules: {
   },
---
+++ src/views/Players.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="players">
+    {{ players }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'Players',
+  props: {
+    msg: String,
+  },
+  created() {
+    this.$store.dispatch('loadPlayers');
+  },
+  computed: {
+    players() {
+      return this.$store.state.players;
+    },
+  },
+};
+</script>
